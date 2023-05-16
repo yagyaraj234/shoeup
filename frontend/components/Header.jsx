125ad3d5e456f7e917d7e0351e3faced96c7861c
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-17 00:02:01 +0530

    header

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, use } from 'react'
 import Wrapper from './Wrapper';
 Wrapper
-
+import Link from 'next/link'
+import Menu from './Menu';
 const Header = () => {
 
   const [mobileMenu, setMobileMenu] = useState(false)
@@ -14,10 +15,11 @@ const Header = () => {
     <header className={`w-full h-[50px] md:h-[80px] bg-white items-center justify-between z-20 sticky top-0 transition-transform dutation-300 ${show} `}>
 
       <Wrapper>
-        <img src='/logo.svg' className='w-[40px] md:w-[60px]' />
+        <Link href="/"><img src='/logo.svg' className="w-[40px] md:w-[60px] " /></Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
       </Wrapper>
     </header>
   )
 }
 
-export default Header
+export default Header;
