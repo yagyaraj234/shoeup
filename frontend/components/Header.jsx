41171575ea2266f2e8eec9a1e0d3a512158736cc
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-16 23:31:14 +0530

    header changes

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -12,8 +12,10 @@ const Header = () => {
 
   return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white items-center justify-between z-20 sticky top-0 transition-transform dutation-300 ${show} `}>
-      
-      <Wrapper/>
+
+      <Wrapper>
+        <img src='/logo.svg' className='w-[40px] md:w-[60px]' />
+      </Wrapper>
     </header>
   )
 }
