e15f2bbaed578154dcb759ee09c16a09b1dea992
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-16 22:49:13 +0530

    header

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect, use } from 'react'
+import Wrapper from './Wrapper';
+Wrapper
 
 const Header = () => {
+
+  const [mobileMenu, setMobileMenu] = useState(false)
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+
   return (
-    <div></div>
+    <header className={`w-full h-[50px] md:h-[80px] bg-white items-center justify-between z-20 sticky top-0 transition-transform dutation-300 ${show} `}>
+      
+      <Wrapper/>
+    </header>
   )
 }
 
