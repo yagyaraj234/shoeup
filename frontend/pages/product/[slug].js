ca7425b75a545a6471ac70259cfe6441b6c0314b
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-20 17:54:15 +0530

    product routing

---
+++ frontend/pages/product/[slug].js
@@ -0,0 +1,27 @@
+const { default: Wrapper } = require("@/components/Wrapper");
+
+import ProductDetailsCrousel from '@/components/ProductDetailsCrousel';
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div className='w-full md:py-20'>
+        <Wrapper>
+        <div className='flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[50px]'>
+            {/* Left Side Start  */}
+            <div className='w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0 '>
+                <ProductDetailsCrousel/>
+            </div>
+             {/* Left Side End  */}
+
+              {/* Right Side Start  */}
+            <div className='flex-[1] py-3'>rigth</div>
+            {/* Right Side End  */}
+        </div>
+        
+        </Wrapper>
+    </div>
+  )
+}
+
+export default ProductDetails
