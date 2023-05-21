530cb5a4af0f153334bd6c5215aec280d4fb0ed3
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-22 00:06:09 +0530

    related component added

--- frontend/components/ProductDetailsCrousel.jsx
+++ frontend/components/ProductDetailsCrousel.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import {Carousel} from 'react-responsive-carousel'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 const ProductDetailsCrousel = () => {
   return (
     <div>
-      <Carousel/>
-    </div>
-  )
+      <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+        <Carousel
+          infiniteLoop={true}
+          showIndicators={false}
+          showStatus={false}
+          thumbWidth={60}
+          className="productCarousel"
+        >
+          {/* {images?.map((img) => (
+            <img
+              key={img.id}
+              src={img.attributes.url}
+              alt={img.attributes.name}
+            />
+          ))} */}
+          <img src="/p2.png" />
+                <img src="/p3.png" />
+                <img src="/p4.png" />
+                <img src="/p5.png" />
+                <img src="/p6.png" />
+                <img src="/p7.png" />
+        </Carousel>
+      </div>
+      </div>
+      )
 }
 
-export default ProductDetailsCrousel;
+      export default ProductDetailsCrousel;
