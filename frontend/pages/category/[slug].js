eeebb935489dca5b892329e04b1228cfcaf782aa
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-29 16:24:34 +0530

    category and products

--- frontend/pages/category/[slug].js
+++ frontend/pages/category/[slug].js
@@ -1,28 +1,53 @@
-import ProductCard from '@/components/ProductCard'
-import Wrapper from '@/components/Wrapper'
-import React from 'react'
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
+import { fetchDataFromApi } from "@/utils/api";
+import React from "react";
 
-
-const Category = () => {
+const Category = ({category,products,slug}) => {
   return (
-    <div className='w-full md:py-20'>
-        <Wrapper>
-            <div className='text-center max-w-[800px] mx-auto mt-8 md:mt-0  '>
-                <div className='text-[28px] md:text-[34px] mb-5 font-semibold leading-tight '>
-                    Running 
-                    shoes
-                </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-
+    <div className="w-full md:py-20">
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0  ">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight ">
+            {category?.data[0]?.attributes?.name}
           </div>
-        </Wrapper>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+        {/* // Mapping the category Product items   */}
+          {products?.data?.map((product)=>(
+            <ProductCard key={product?.id} data={product}/>
+          ))}
+
+        </div>
+      </Wrapper>
     </div>
-  )
+  );
+};
+
+export default Category;
+
+export async function getStaticPaths() {
+  const category = await fetchDataFromApi("/api/categories?populate=*");
+  const paths = category.data.map((c) => ({
+    params: {
+      slug: c.attributes.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export default Category
+export async function getStaticProps({ params: { slug } }) {
+  const category = await fetchDataFromApi(`/api/categories?filters[slug][$eq]=${slug}`);
+  const products = await fetchDataFromApi(`/api/products?populate=*&[filters][categories][slug][$eq]=${slug}`);
+
+  return {
+    props:{
+      category,
+      products,
+      slug
+    }
+  }
+}
--- frontend/pages/product/[slug].js
+++ frontend/pages/product/[slug].js
@@ -1,38 +1,36 @@
-import ProductDetailsCrousel from '@/components/ProductDetailsCrousel';
-import React from 'react'
-import Wrapper from '@/components/Wrapper';
-import { IoMdHeartEmpty } from 'react-icons/io';
+import ProductDetailsCrousel from "@/components/ProductDetailsCrousel";
+import React, { useState } from "react";
+import Wrapper from "@/components/Wrapper";
+import { IoMdHeartEmpty } from "react-icons/io";
 import ReactMarkdown from "react-markdown";
-import RelatedProducts from '@/components/RelatedProducts'
+import RelatedProducts from "@/components/RelatedProducts";
+import { fetchDataFromApi } from "@/utils/api";
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+  const [selectedSize, setSelectedSize] = useState();
+  const [showError, setShowError] = useState(false);
+
+  const p = product?.data?.[0]?.attributes;
   return (
-    <div className='w-full md:py-20'>
-        <Wrapper>
-        <div className='flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[50px]'>
-            {/* Left Side Start  */}
-            <div className='w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0 '>
-                <ProductDetailsCrousel/>
-            </div>
-             {/* Left Side End  */}
+    <div className="w-full md:py-20">
+      <Wrapper>
+        <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[50px]">
+          {/* Left Side Start  */}
+          <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0 ">
+            <ProductDetailsCrousel images={p.image.data} />
+          </div>
+          {/* Left Side End  */}
 
-              {/* Right Side Start  */}
-            <div className='flex-[1] py-3'>
-
-              {/* Product NAme  */}
-            <div className="text-[34px] font-semibold mb-2">
-              Jordan Retro 6 G
-            </div>
+          {/* Right Side Start  */}
+          <div className="flex-[1] py-3">
+            {/* Product NAme  */}
+            <div className="text-[34px] font-semibold mb-2">{p.name}</div>
 
             {/* Product Subtitle  */}
-            <div className="text-lg font-semibold mb-5">
-              Men&apos;s Golf Shoes
-            </div>
+            <div className="text-lg font-semibold mb-5">{p.subtitle}</div>
 
             {/* Product Price  */}
-            <div className="text-lg font-semibold">
-              MRP : $ 16 695.00
-            </div>
+            <div className="text-lg font-semibold">&#8377; {p.price}</div>
             <div className="text-md font-medium text-black/[0.5]">
               incl. of taxes
             </div>
@@ -40,95 +38,115 @@ const ProductDetails = () => {
               {`(Also includes all applicable duties)`}
             </div>
 
-             {/* PRODUCT SIZE RANGE START */}
-             <div className="mb-10">
-                            {/* HEADING START */}
-                            <div className="flex justify-between mb-2">
-                                <div className="text-md font-semibold">
-                                    Select Size
-                                </div>
-                                <div className="text-md font-medium text-black/[0.5] cursor-pointer">
-                                    Select Guide
-                                </div>
-                            </div>
-                            {/* HEADING END */}
+            {/* PRODUCT SIZE RANGE START */}
+            <div className="mb-10">
+              {/* HEADING START */}
+              <div className="flex justify-between mb-2">
+                <div className="text-md font-semibold">Select Size</div>
+                <div className="text-md font-medium text-black/[0.5] cursor-pointer">
+                  Select Guide
+                </div>
+              </div>
+              {/* HEADING END */}
 
-                            {/* Size Start  */}
+              {/* Size Start  */}
 
-                            <div
-                                id="sizesGrid"
-                                className="grid grid-cols-3 gap-2"
-                            >
+              <div id="sizesGrid" className="grid grid-cols-3 gap-2">
+                {p.size.data.map((item, i) => (
+                  <div
+                    key={i}
+                    className={`border rounded-md text-center py-3 font-medium  ${
+                      item.enabled
+                        ? "hover:border-black cursor-pointer"
+                        : "cursor-not-allowed bg-black/[0.1] opacity-50" 
+                    } ${selectedSize === item.size? 'border-black':''}`}
+                    onClick={() => {
+                      setSelectedSize(item.size);
+                      setShowError(false)
+                    }}
+                  >
+                    {item.size}
+                  </div>
+                ))}
+              </div>
+              {/* Size End  */}
 
-                            <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                              UK 6
-                            </div>
-                            <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                              UK 7
-                            </div>
-                            <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                              UK 8
-                            </div>
-                            <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                              UK 9
-                            </div>
-                            <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                              UK 10
-                            </div>
-                            <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer ">
-                              UK 11
-                            </div>
-                            <div className="border rounded-md text-center py-3 font-medium hover:border-black  cursor-not-allowed bg-black/[0.1] opacity-50">
-                              UK 12
-                            </div>
+              {/* Size Error Message Start  */}
 
-                            </div>
-                            {/* Size End  */}
+              {showError && (
+                <div className="text-red-600 mt-1">
+                  Size Selection is required
+                </div>
+              )}
+              {/* Size Error Message End  */}
 
-                            {/* Size Error Message Start  */}
+              <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75" onClick={()=>{
+                if(!selectedSize){
+                  setShowError(true)
+                  document.getElementById("sizesGrid").scrollIntoView({
+                    block:"center",
+                    behavior:"smooth"
+                  })
+                }
+              }}>
+                Add to Cart
+              </button>
+              {/* ADD TO CART BUTTON END */}
 
-                            <div className="text-red-600 mt-1">
-                              Size Selection is required
-                            </div>
-                            {/* Size Error Message End  */}
+              {/* WHISHLIST BUTTON START */}
+              <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
+                Whishlist
+                <IoMdHeartEmpty size={20} />
+              </button>
+              {/* WHISHLIST BUTTON END */}
 
-                            <button
-                            className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
-                            
-                        >
-                            Add to Cart
-                        </button>
-                        {/* ADD TO CART BUTTON END */}
-
-                        {/* WHISHLIST BUTTON START */}
-                        <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
-                            Whishlist
-                            <IoMdHeartEmpty size={20} />
-                        </button>
-                        {/* WHISHLIST BUTTON END */}
-
-                        <div>
-                            <div className="text-lg font-bold mb-5">
-                                Product Details
-                            </div>
-                            <div className="markdown text-md mb-5">
-                                <ReactMarkdown> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam ipsum architecto vel fugit, consequatur modi placeat quidem reiciendis dignissimos repudiandae nobis quod? Quasi suscipit veritatis minus quaerat incidunt excepturi sunt expedita repudiandae debitis consectetur?</ReactMarkdown>
-                            </div>
-                        </div>
-                    </div>
-
-
-
+              <div>
+                <div className="text-lg font-bold mb-5">Product Details</div>
+                <div className="markdown text-md mb-5">
+                  <ReactMarkdown>
+                    {/* {" "} */}
+                    {p.description}
+                  </ReactMarkdown>
+                </div>
+              </div>
             </div>
-            {/* Right Side End  */}
+          </div>
+          {/* Right Side End  */}
         </div>
-  
-        <RelatedProducts  />
 
-
-        </Wrapper>
+        <RelatedProducts  products={products} />
+      </Wrapper>
     </div>
   );
+};
+
+export default ProductDetails;
+
+export async function getStaticPaths() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+  const paths = products.data.map((p) => ({
+    params: {
+      slug: p.attributes.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export default ProductDetails
+export async function getStaticProps({ params: { slug } }) {
+  const product = await fetchDataFromApi(
+    `/api/products?populate=*&filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filter][slug][$ne]=${slug}`
+  );
+
+  return {
+    props: {
+      product,
+      products,
+    },
+  };
+}
