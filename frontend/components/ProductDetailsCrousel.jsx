a372dbcaa7e395e243c33fd010684a03f4073eae
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-29 16:23:58 +0530

    product detail connected to db

--- frontend/components/ProductDetailsCrousel.jsx
+++ frontend/components/ProductDetailsCrousel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-const ProductDetailsCrousel = () => {
+const ProductDetailsCrousel = ({images}) => {
   return (
     <div>
       <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
@@ -13,19 +13,11 @@ const ProductDetailsCrousel = () => {
           thumbWidth={60}
           className="productCarousel"
         >
-          {/* {images?.map((img) => (
-            <img
-              key={img.id}
-              src={img.attributes.url}
-              alt={img.attributes.name}
-            />
-          ))} */}
-          <img src="/p2.png" />
-                <img src="/p3.png" />
-                <img src="/p4.png" />
-                <img src="/p5.png" />
-                <img src="/p6.png" />
-                <img src="/p7.png" />
+          
+          {images?.map((img)=>(
+            <img src={img.attributes.url} alt={img.attributes.name}/>
+          ))}
+          
         </Carousel>
       </div>
       </div>
