d2c3944b6ba656ec34f229e41635bcddf1c123b3
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-29 16:25:01 +0530

    urls

--- frontend/utils/urls.js
+++ frontend/utils/urls.js
@@ -1,3 +1,4 @@
-export const STRAPI_API_TOKEN =process.env.NEXT_PUBLIC_STRAPI_API_TOKEN 
+export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
-export const API_URL =process.env.NEXT_PUBLIC_API_URL || 'https://127.0.0.1:1337';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:1337";
