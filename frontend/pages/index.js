e7b858ac9d8911df1e1fdffca7671d3b43751b87
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-29 16:22:50 +0530

    error

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,40 +1,57 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import HeroBanner from '../components/HeroBanner'
-import Wrapper from '@/components/Wrapper'
-import ProductCard from '@/components/ProductCard'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import HeroBanner from "../components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
+import ProductCard from "@/components/ProductCard";
+import { useEffect, useState } from "react";
+import { fetchDataFromApi } from "@/utils/api";
 
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function Home({ products }) {
   return (
     <>
-      <main className='md:px-[150px]'  >
-        <HeroBanner/>
+      <main className="md:px-[150px]">
+        <HeroBanner />
         <Wrapper>
           {/* Heading and Paragraph Start  */}
-          <div className='text-center max-w-[800px] mx-auto
-           my-[50px] md:my-[80px] '>
-            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">Cushioning for Your Miles</div>
-            <div className="text-md md:text-xl">A lightweight Nike ZoomX midsole is combined with
-                        increased stack heights to help provide cushioning
-                        during extended stretches of running.</div>
+          <div
+            className="text-center max-w-[800px] mx-auto
+           my-[50px] md:my-[80px] "
+          >
+            <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+              Cushioning for Your Miles
+            </div>
+            <div className="text-md md:text-xl">
+              A lightweight Nike ZoomX midsole is combined with increased stack
+              heights to help provide cushioning during extended stretches of
+              running.
+            </div>
           </div>
           {/* Heading and Paragraph End  */}
 
-        {/* Product Grid  */}
+          {/* Product Grid  */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-
+            {products.data.map((product) => (
+              <ProductCard key={product?.id} data={product}/>
+            ))}
+            {/* <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard /> */}
           </div>
         </Wrapper>
       </main>
     </>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
 }
