a4c50e02fea7a0a397c3ba66983ccf6aa537ee05
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-10 10:15:44 +0530

    backend connnection

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,3 +1,4 @@
+import { stringify } from "postcss";
 import { API_URL, STRAPI_API_TOKEN } from "./urls";
 
 export const fetchDataFromApi = async (endpoint) => {
@@ -8,8 +9,21 @@ export const fetchDataFromApi = async (endpoint) => {
     },
   };
 
-  const res = await fetch(`${API_URL}${endpoint}`,options);
+  const res = await fetch(`${API_URL}${endpoint}`, options);
   const data = await res.json();
 
+  return data;
+};
+
+export const makePaymentRequest = async (endpoint, payload) => {
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + STRAPI_API_TOKEN,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await res.json();
   return data;
 };
