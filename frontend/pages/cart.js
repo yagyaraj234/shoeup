624d8fc75015fd96eddbc286e78361de790f2a6d
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-10 10:15:55 +0530

    checkout added

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -5,11 +5,33 @@ import Link from "next/link";
 import CartItem from "@/components/CartItem";
 import { useSelector } from "react-redux";
 
+import { loadStripe } from "@stripe/stripe-js";
+import { makePaymentRequest } from "@/utils/api";
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
 const Cart = () => {
+  const [loading, setLoading] = useState(false);
   const { cartItems } = useSelector((state) => state.cart);
+
   const subTotal = useMemo(() => {
     return cartItems.reduce((total, val) => total + val.attributes.price, 0);
   }, [cartItems]);
+
+  const handlePayment = async () => {
+    try {
+      setLoading(true);
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest("/api/orders", {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.stripeSession.id,
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
   return (
     <div className="w-full md:pt-5 md:px-[150px]">
       <Wrapper>
@@ -56,8 +78,12 @@ const Cart = () => {
                 </div>
 
                 {/* BUTTON START */}
-                <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center">
+                <button
+                  className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center "
+                  onClick={handlePayment}
+                >
                   Checkout
+                  {loading && <img src="/spinner.svg" />}
                 </button>
                 {/* BUTTON END */}
               </div>
