bf7c2b1ff75193498d682c713a2f4b88726c9183
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-23 16:11:20 +0530

    Revert "persist added"

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,19 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
-import Storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
 
-const persistConfig = {
-  key: "isom",
-  Storage,
-};
-
-const persistCartReducer = persistReducer(persistConfig, cartSlice);
-
-const store = configureStore({
+export default configureStore({
   reducer: {
-    persistCartReducer,
+    cart:cartSlice,
   },
 });
-export default store;
-export const persistor = persistStore(store);
